82c1a83175f28ebe59c24a51de0625870f2cb4b7
Palash Johri <palashjohricool@gmail.com>
2023-03-16 18:12:29 +0530

    added environment variable support, cors origin to vue app

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 app = Flask(__name__)
+cors = CORS(app, supports_credentials=True,
+            origins=os.environ.get("FRONTEND_URL"))
+app.config["CORS_HEADERS"] = "Content-Type"
 
 
 @app.route("/", methods=["GET"])
+@cross_origin()
 def helloworld():
     if request.method == "GET":
         data = {"data": "Hello from Flask!"}
